e3f6e00e982e1c79df13045478d0ba3245f09e4c
Mohammed Saqlain <md.saqlain3100@gmail.com>
2024-10-29 20:47:52 +0530

    Dasgboard route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInSide from "./components/admin/SignIn/SignInSide";
-``
+import Dashboard from "./components/admin/dashboard/Dashboard";
 function App() {
   return (
     <Router >
       <Routes>
         <Route path="/login" element={<SignInSide/>}/>
+        <Route path="/admin/dashboard" element={<Dashboard/>}/>
       </Routes>
     </Router>
   );
