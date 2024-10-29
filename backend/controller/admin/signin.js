412c8f835f87770c3eeaca31c93981474a957698
Nirosh Shetty <niroshshetty2020@gmail.com>
2024-10-29 20:07:02 +0530

    adminSignIn updated

--- backend/controller/admin/signin.js
+++ backend/controller/admin/signin.js
@@ -1,12 +1,34 @@
 import express from "express";
 const app = express();
 // import adminModel from "../model/adminModel";
+import adminModel from "../../model/adminModel.js";
+import bcrypt from "bcrypt";
 
 export const adminSigin = async (req, res) => {
   console.log("admin sigin hitted");
+  const { identifier, passowrd } = req.body();
   try {
-    res.json({
-      message: "admin hitted",
+    const user = await adminModel.findOne({
+      $or: [{ username: identifier }, { email: identifier }],
     });
-  } catch (error) {}
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+    const comPassword = await bcrypt.compare(passowrd, user.passowrd);
+    if (!comPassword) {
+      return res.status(401).json({
+        message: "Incorrect Password",
+      });
+    }
+
+    return res.status(201).json({
+      message: "Admin Signed In Successfully",
+    });
+  } catch (error) {
+    return res.status(501).json({
+      message: "Failed to SignIn",
+    });
+  }
 };
--- backend/model/adminModel.js
+++ backend/model/adminModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const adminSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
