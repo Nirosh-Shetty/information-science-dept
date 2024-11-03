a505a938d266f5d6ee63f2fddeffd10c9dd148a9
Nirosh Shetty <niroshshetty2020@gmail.com>
2024-11-03 15:22:40 +0530

    added signin options page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInSide from "./components/admin/SignIn/SignInSide";
 import Dashboard from "./components/admin/dashboard/Dashboard";
 import { Home } from "./components/home/Home";
+import SignInOptions from "./components/siginOptions/SignInOptions";
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/signinoptions" element={<SignInOptions />} />
         <Route path="/" element={<Home></Home>} />
         <Route path="/admin/signin" element={<SignInSide />} />
         <Route path="/admin/dashboard" element={<Dashboard />} />
