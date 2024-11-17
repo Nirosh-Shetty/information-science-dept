4bf1df3ec3e5725294232824f0d94dad3f741f80
Mohammed Saqlain <md.saqlain3100@gmail.com>
2024-11-18 01:24:12 +0530

    fixed routes

--- backend/routes/indexRoutes.js
+++ backend/routes/indexRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import adminRoute from "./adminRoutes.js";
 import studentrRoute from "./studentRoutes.js";
 import staffRoute from "./staffRoutes.js";
+import courseRoute from "./courseRoutes.js";
 import { signIn } from "../controller/signIn.js";
 import { authorizeUser } from "../controller/authoriseUser.js";
 import jwtMiddleware from "../middleware/jwtMiddleware.js";
@@ -10,6 +11,7 @@ import jwtMiddleware from "../middleware/jwtMiddleware.js";
 router.use("/admin", adminRoute);
 router.use("/student", studentrRoute);
 router.use("/staff", staffRoute);
+router.use("/courses", courseRoute);
 router.post("/signin", signIn);
 router.post("/authoriseuser", jwtMiddleware, authorizeUser);
 
