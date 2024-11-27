eb4d9786f913db7a393562b6326c297839010f0f
Nirosh Shetty <niroshshetty2020@gmail.com>
2024-11-27 09:36:32 +0530

    updated select class

--- frontend/src/components/staff/attendance/SelectClass.jsx
+++ frontend/src/components/staff/attendance/SelectClass.jsx
@@ -9,26 +9,41 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { staffAtom } from "../../../../recoil/atoms/staffAtom";
 import { useRecoilState } from "recoil";
+import {
+  classAtom,
+  currentSelectedClass as currentSelectedClassAtom,
+} from "../../../../recoil/atoms/classAtom";
 
 export default function SelectClass() {
-  const [staff, setStaff] = useRecoilState(staffAtom);
-  console.log("shjhhhhhh", staff);
+  const [classes, setClasses] = useRecoilState(classAtom);
+  const [currentSelectedClass, setCurrentSelectedClass] = useRecoilState(
+    currentSelectedClassAtom
+  );
+  const handleValueChange = (value) => {
+    console.log(value);
+    const selectedClass = classes.find((item) => item._id === value);
+    setCurrentSelectedClass(selectedClass);
+  };
+  React.useEffect(() => {
+    //ftech the attendence list
+  }, [currentSelectedClass]);
+
+  // console.log(classes);
   return (
     <div className="w-8/9 my-5">
-      <Select className="w-full">
-        <SelectTrigger className="text-xl p-5 text-gray-600 font-semibold dark:text-gray-100 dark:border-2 ">
+      <Select className="w-full" onValueChange={handleValueChange}>
+        <SelectTrigger className="text-xl p-5 text-gray-600 font-semibold dark:text-gray-100 dark:border-2">
           <SelectValue placeholder="Select a Class" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup className="text-2xl">
             <SelectLabel>Class</SelectLabel>
-            <SelectItem value="apple">hhhhhhh</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
+            {classes.map((item, index) => (
+              <SelectItem key={item._id} value={item._id}>
+                {item.name}
+              </SelectItem>
+            ))}
           </SelectGroup>
         </SelectContent>
       </Select>
